9964a26297a35975d018e8144f58d699ef0ee020
Minnan Qiu <minnanqcu@hotmail.com>
2024-11-06 20:29:30 -0500

    mq2254

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -31,8 +31,8 @@ function increase_score(id){
     dataType : "json",
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
-    success: function(result){
-        
+    success: function(updated_scoreboard){
+        display_scoreboard(updated_scoreboard);
     },
     error: function(request, status, error){
         console.log("Error");
